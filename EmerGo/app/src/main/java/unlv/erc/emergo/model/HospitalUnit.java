a7090f6403b0f24d3f7e0f90f9a5d9c9ce2c2c84
LeonardoRK <leodegolim@yahoo.com.br>
2016-04-16 19:22:06 -0300

    UnityHospitalClass.java

--- EmerGo/app/src/main/java/unlv/erc/emergo/model/HospitalUnit.java
+++ EmerGo/app/src/main/java/unlv/erc/emergo/model/HospitalUnit.java
@@ -5,4 +5,58 @@ package unlv.erc.emergo.model;
  */
 public class HospitalUnit {
 
+    private Float latitude;
+    private Float longitude;
+    private String endereco;
+    private Double telefone;
+
+    public HospitalUnit(Float latitude, Float longitude, String endereco, Double telefone) {
+        this.latitude = latitude;
+        this.longitude = longitude;
+        this.endereco = endereco;
+        this.telefone = telefone;
+    }
+
+    public Float getLatitude() {
+        return latitude;
+    }
+
+    public Float getLongitude() {
+        return longitude;
+    }
+
+    public String getEndereco() {
+        return endereco;
+    }
+
+    public Double getTelefone() {
+        return telefone;
+    }
+
+    public void setLatitude(Float latitude) {
+        this.latitude = latitude;
+    }
+
+    public void setLongitude(Float longitude) {
+        this.longitude = longitude;
+    }
+
+    public void setEndereco(String endereco) {
+        this.endereco = endereco;
+    }
+
+    public void setTelefone(Double telefone) {
+        this.telefone = telefone;
+    }
+
+    @Override
+    public String toString() {
+        return "Hospitais{" +
+                "latitude=" + latitude +
+                ", longitude=" + longitude +
+                ", endereco='" + endereco + '\'' +
+                ", telefone=" + telefone +
+                '}';
+    }
+
 }
