6056e135ece65ba70080d6815d0cc937fb275d48
Naiara <andradenaiara@hotmail.com>
2016-04-17 17:10:23 -0300

    adding attributes, class builders and attributes getters and setters

--- EmerGo/app/src/main/java/unlv/erc/emergo/model/User.java
+++ EmerGo/app/src/main/java/unlv/erc/emergo/model/User.java
@@ -1,7 +1,93 @@
 package unlv.erc.emergo.model;
 
+import java.util.Date;
+
 /**
  * Created by naiara on 17/04/16.
  */
 public class User {
+
+    private String name = "";
+    private Date birthday;
+    private String typeBlood = "";
+    private String allergy = "";
+    private boolean cardiac = false;
+    private boolean diabetic = false;
+    private boolean hypertension = false;
+    private boolean seropositive = false;
+
+    public User(){
+
+    }
+
+    public User(String name, Date birthday, String typeBlood){
+        this.name = name;
+        this.birthday = birthday;
+        this.typeBlood = typeBlood;
+    }
+
+    public String getName(){
+        return name;
+    }
+
+    public void setName(String name){
+        this.name = name;
+    }
+
+    public Date getBirthday(){
+        return birthday;
+    }
+
+    public void setBirthday(Date birthday){
+        this.birthday = birthday;
+    }
+
+    public String getTypeBlood(){
+        return typeBlood;
+    }
+
+    public void setTypeBlood(String typeBlood){
+        this.typeBlood = typeBlood;
+    }
+
+    public String getAllergy(){
+        return allergy;
+    }
+
+    public void setAllergy(String allergy){
+        this.allergy = allergy;
+    }
+
+    public boolean getCardiac(){
+        return cardiac;
+    }
+
+    public void setCardiac(boolean cardiac){
+        this.cardiac = cardiac;
+    }
+
+    public boolean getDiabetic(){
+        return diabetic;
+    }
+
+    public void setDiabetic(boolean diabetic){
+        this.diabetic = diabetic;
+    }
+
+    public boolean getHypertension(){
+        return hypertension;
+    }
+
+    public void setHypertension(boolean hypertension){
+        this.hypertension = hypertension;
+    }
+
+    public boolean getSeropositive(){
+        return seropositive;
+    }
+
+    public void setSeropositive(boolean seropositive){
+        this.seropositive = seropositive;
+    }
+
 }
