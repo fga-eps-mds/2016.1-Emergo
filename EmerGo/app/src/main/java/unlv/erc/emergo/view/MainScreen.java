f2c3457918ad05a6a06f267e45bde978d53b40b5
Matheus <matheusfp2007@yahoo.com.br>
2016-04-22 01:44:25 -0300

    XML da tela inicial

---
+++ EmerGo/app/src/main/java/unlv/erc/emergo/view/MainScreen.java
@@ -0,0 +1,18 @@
+package unlv.erc.emergo.view;
+
+import android.os.Bundle;
+
+import unlv.erc.emergo.MainActivity;
+import unlv.erc.emergo.R;
+
+/**
+ * Created by Matheus on 21/04/2016.
+ */
+public class MainScreen extends MainActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main_screen);
+    }
+}
