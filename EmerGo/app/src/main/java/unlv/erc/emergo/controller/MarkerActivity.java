cd8d681905d6b29e12704d1c3c195cfe68040f5f
gabrielrf97 <gabrielrodriguesfaria97@gmail.com>
2016-04-28 11:56:03 -0300

    classe Marker - controller

---
+++ EmerGo/app/src/main/java/unlv/erc/emergo/controller/MarkerActivity.java
@@ -0,0 +1,42 @@
+package unlv.erc.emergo.controller;
+
+import android.Manifest;
+import android.content.Context;
+import android.content.pm.PackageManager;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
+
+import com.google.android.gms.location.LocationServices;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
+
+import java.util.ArrayList;
+
+/**
+ * Created by gabriel on 27/04/16.
+ */
+public class MarkerActivity {
+
+    //The use od this function requires the method onMapReady
+    protected void setMarkerOnMap(GoogleMap map,double lat,double lng, string title) {
+       map.addMarker(new MarkerOptions()
+               .position(new LatLng(lat,lng))
+               .title("%s",title);
+    }
+
+    protected void setUnvisible(Marker marker){
+        marker.setVisible(false);
+    }
+   /* This method should be used to add many Markers on map
+    protected void setMarkersOnMap(GoogleMap map,ArrayList<Marker> markers){
+         //WE should define how many markers we shall show to the user
+         int markersQuantity;
+        for(markersQuantity = 0; markersQuantity < markers.size(); markersQuantity++){
+            map.addMarker(new MarkerOptions()
+                    .position(new LatLng(markers[markersQuantity].lat,markers[markersQuantity].lng))
+                    .title("%s",markers[markersQuantity].title);
+        }*/
+    }
+}
