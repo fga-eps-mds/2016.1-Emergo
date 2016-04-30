8de2332457e9c28b729eacff3901df8046bf32df
André Bedran <bedran.fleck@gmail.com>
2016-04-30 15:17:14 -0300

    Refactoring project files and gradle build files.

--- EmerGo/app/src/main/java/unlv/erc/emergo/controller/MarkerActivity.java
+++ EmerGo/app/src/main/java/unlv/erc/emergo/controller/MarkerActivity.java
@@ -1,20 +1,9 @@
 package unlv.erc.emergo.controller;
 
-import android.Manifest;
-import android.content.Context;
-import android.content.pm.PackageManager;
-import android.location.Location;
-import android.location.LocationListener;
-import android.location.LocationManager;
-
 import com.google.android.gms.location.LocationServices;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
-
-import java.util.ArrayList;
-
-import unlv.erc.emergo.model.HealthUnit;
 
 /**
  * Created by gabriel on 27/04/16.
