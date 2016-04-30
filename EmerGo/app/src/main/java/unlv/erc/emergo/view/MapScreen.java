758785e4a02c02ca586eb37fca9399577fafd684
Matheus <matheusfp2007@yahoo.com.br>
2016-04-29 21:16:34 -0300

    Configuracoes de views

---
+++ EmerGo/app/src/main/java/unlv/erc/emergo/view/MapScreen.java
@@ -0,0 +1,20 @@
+package unlv.erc.emergo.view;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+import unlv.erc.emergo.R;
+
+/**
+ * Created by Matheus on 24/04/2016.
+ */
+public class MapScreen extends Activity {
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.map_screen);
+
+    }
+}
