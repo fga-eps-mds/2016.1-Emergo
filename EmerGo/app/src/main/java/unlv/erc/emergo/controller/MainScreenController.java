f064a55463bb396a4294f890b5e91caffafa5e4d
André Bedran <bedran.fleck@gmail.com>
2016-05-20 17:06:50 -0300

    Database fetch&save method working. Setiing OnClickListeners individually to each button.

--- EmerGo/app/src/main/java/unlv/erc/emergo/controller/MainScreenController.java
+++ EmerGo/app/src/main/java/unlv/erc/emergo/controller/MainScreenController.java
@@ -1,10 +1,12 @@
 package unlv.erc.emergo.controller;
 
 import android.app.Activity;
+import android.app.Service;
 import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.view.View.OnClickListener;
 import android.widget.Button;
 import android.widget.Toast;
 
@@ -35,24 +37,45 @@ public class MainScreenController extends Activity {
         Log.i("log", "FIIM ALELUIA ALELIA KAKAKAK");
 
 
+        OnClickListener goListener = new OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                //TO-DO!
+
+            }
+        };
+
+        OnClickListener okayListener = new OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                Services services = new Services();
+
+                services.selectHealhUnitys(services.getUserPosition());
+
+                Intent mapScreen = new Intent();
+                mapScreen.setClass(getBaseContext(), MapScreenController.class);
+                startActivity(mapScreen);
+
+            }
+        };
+
         setContentView(R.layout.main_screen);
         goButton = (Button) findViewById(R.id.buttonGo);
+        goButton.setOnClickListener(goListener);
         fineButton = (Button) findViewById(R.id.buttonOkay);
+        fineButton.setOnClickListener(okayListener);
     }
 
     public void goClicked(View main_Screen){
 
-        Toast.makeText(this , "Função não habilitada!" , Toast.LENGTH_SHORT).show();
+
     }
 
     public void okayClicked(View main_Screen){
 
-        this.services = new Services();
-      //  this.services.selectHealhUnitys(services.getUserPosition());
 
-        Intent mapScreen = new Intent();
-        mapScreen.setClass(this, MapScreenController.class);
-        startActivity(mapScreen);
     }
 
 }
