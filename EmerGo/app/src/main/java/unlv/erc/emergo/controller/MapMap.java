c96d10b6538e1f46db6252aa16aa4c90ff99d345
LeonardoRK <leodegolim@yahoo.com.br>
2016-05-27 15:36:24 -0300

    Refactoring methods in class MapMap.java
    Signed-off-by: LeonardoRK <leodegolim@yahoo.com.br>
    Signed-off-by: marcosddourado <marcosdourado92@gmail.com>

--- EmerGo/app/src/main/java/unlv/erc/emergo/controller/MapMap.java
+++ EmerGo/app/src/main/java/unlv/erc/emergo/controller/MapMap.java
@@ -5,6 +5,7 @@ import android.app.Activity;
 import android.content.Intent;
 import android.location.Location;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.v4.app.FragmentActivity;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
@@ -23,7 +24,7 @@ import unlv.erc.emergo.R;
 
 public class MapMap extends Activity {
 
-    public static final int ZOOM_LEVEL = 13; 
+    public static final int ZOOM_LEVEL = 13;
 
     @Override public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -31,22 +32,30 @@ public class MapMap extends Activity {
 
         GPSTracker gps = new GPSTracker(this);
         Location userLocation = gps.getLocation();
+
         Log.i("latitude", userLocation.getLatitude() + "");
         Log.i("Longitude", userLocation.getLongitude() + "");
 
-        MapView map = (MapView) findViewById(R.id.map);
-        map.setTileSource(TileSourceFactory.MAPNIK);
+        MapView map = setMapOnActivity();
 
-        map.setBuiltInZoomControls(true);
-        map.setMultiTouchControls(true);
+        setInitialZoomLevel(userLocation, map);
+    }
 
+    private void setInitialZoomLevel(Location userLocation, MapView map) {
         IMapController mapController = map.getController();
         mapController.setZoom(ZOOM_LEVEL);
         GeoPoint userGeoPoint = new GeoPoint(userLocation.getLatitude(),
                                             userLocation.getLongitude());
         mapController.setCenter(userGeoPoint);
+    }
 
-
+    @NonNull
+    private MapView setMapOnActivity() {
+        MapView map = (MapView) findViewById(R.id.map);
+        map.setTileSource(TileSourceFactory.MAPNIK);
+        map.setBuiltInZoomControls(true);
+        map.setMultiTouchControls(true);
+        return map;
     }
 
     public void goClicked(View map_screen) {
