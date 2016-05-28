d26ff0742f04b4870b4089d74fc798117a9e9cc5
LeonardoRK <leodegolim@yahoo.com.br>
2016-05-28 16:22:29 -0300

    new CustomResourceProxy class. Used to power new map engine
    Signed-off-by: LeonardoRK <leodegolim@yahoo.com.br>
    Signed-off-by: gabrielrf97 <gabrielrodriguesfaria97@gmail.com>

---
+++ EmerGo/app/src/main/java/unlv/erc/emergo/controller/CustomResourceProxy.java
@@ -0,0 +1,41 @@
+package unlv.erc.emergo.controller;
+
+import android.content.Context;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.drawable.Drawable;
+
+import org.osmdroid.DefaultResourceProxyImpl;
+
+import unlv.erc.emergo.R;
+
+/**
+ * Created by leonardo on 28/05/16.
+ */
+public class CustomResourceProxy extends DefaultResourceProxyImpl {
+
+    private final Context mContext;
+    public CustomResourceProxy(Context pContext) {
+        super(pContext);
+        mContext = pContext;
+    }
+
+    @Override
+    public Bitmap getBitmap(final bitmap pResId) {
+        switch (pResId){
+            case person:
+                //your image goes here!!!
+                return BitmapFactory.decodeResource(mContext.getResources(), R.drawable.person);
+        }
+        return super.getBitmap(pResId);
+    }
+
+    @Override
+    public Drawable getDrawable(final bitmap pResId) {
+        switch (pResId){
+            case person:
+                return mContext.getResources().getDrawable(R.drawable.person);
+        }
+        return super.getDrawable(pResId);
+    }
+}
