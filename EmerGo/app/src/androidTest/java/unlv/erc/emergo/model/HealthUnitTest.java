65539dce65ce3928e836bb4f3b2472b971f7a474
naiieandrade <andradenaiara@hotmail.com>
2016-05-27 21:38:07 -0300

    "test get name health unit and set empty"
    Signed-off-by: naiieandrade <andradenaiara@hotmail.com>
    MrVictor42 <guin-hu@hotmail.com>
    Matheusss03 <matheusfp2007@yahoo.com.br>

--- EmerGo/app/src/androidTest/java/unlv/erc/emergo/model/HealthUnitTest.java
+++ EmerGo/app/src/androidTest/java/unlv/erc/emergo/model/HealthUnitTest.java
@@ -5,5 +5,18 @@ import junit.framework.TestCase;
 
 public class HealthUnitTest extends TestCase{
 
+    public void testGetNameHealthUnit(){
+        HealthUnit healthUnit = new HealthUnit();
+        String nameHospital = "Hospital Regional do Gama";
+        healthUnit.setUnitType("Hospital Regional do Gama");
+        assertEquals(nameHospital,healthUnit.getUnitType());
+    }
+
+    public void testSetHealthUnitEmpty(){
+        HealthUnit healthUnit = new HealthUnit();
+        healthUnit.setNameHospital("");
+        assertEquals("", healthUnit.getNameHospital());
+    }
+    
 
 }
