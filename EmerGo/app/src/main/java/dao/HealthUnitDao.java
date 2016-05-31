378a7e3ffd4d9f76ccd1fb6e9ed69de9aca6c1e1
MrVictor42 <guin-hu@hotmail.com>
2016-05-31 11:25:23 -0300

    healthunit dao

--- EmerGo/app/src/main/java/dao/HealthUnitDao.java
+++ EmerGo/app/src/main/java/dao/HealthUnitDao.java
@@ -1,11 +1,12 @@
 package dao;
 
-import android.app.Activity;
 import android.content.ContentValues;
 import android.content.Context;
 import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
+
+import unlv.erc.emergo.model.HealthUnit;
 
 
 public class HealthUnitDao extends SQLiteOpenHelper{
@@ -14,25 +15,24 @@ public class HealthUnitDao extends SQLiteOpenHelper{
 
     public static final String HEALTHUNIT_TABLE = "HealthUnit";
     public static final String DROP_TABLE_HEALTHUNIT = "DROP TABLE IF EXISTS" + HEALTHUNIT_TABLE;
-    private Activity activity;
+
     //HealthUnit data
     public static final String LATITUDE = "[latitude]";
     public static final String LONGITUDE = "[longitude]";
     public static final String NAMEHOSPITAL = "[nameHospital]";
     public static final String UNITTYPE = "[unitType]";
-    public static final String ADDRESSNUMBER = "[addressNumber]";
+    public static final String ZIPCODE = "[zipCode]";
     public static final String DISTRICT = "[district]";
     public static final String STATE = "[state]";
     public static final String CITY = "[city]";
     public static final String HEALTHUNIT_ID = "[IDHealthUnit]";
 
     public static final String CREATE_HEALTHUNIT = "CREATE TABLE " + HEALTHUNIT_TABLE + " (" +
-            HEALTHUNIT_ID + "INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT," +
+            ZIPCODE + "INTEGER PRIMARY KEY, "+
             LATITUDE + " FLOAT, "+
             LONGITUDE + " DOUBLE, "+
             NAMEHOSPITAL + " VARCHAR(50), "+
             UNITTYPE + " VARCHAR(42), "+
-            ADDRESSNUMBER + " VARCHAR(8), "+
             DISTRICT + "VARCHAR(42)," +
             STATE + "VARCHAR(42),"+
             CITY + "VARCHAR(442)); ";
@@ -53,20 +53,17 @@ public class HealthUnitDao extends SQLiteOpenHelper{
         onCreate(database);
     }
 
-    public boolean insertHealthUnit(Integer id,Double latitude,Double longitude,String nameHospital,
-                                    String unitType,String addressNumber,String district,String state,
-                                    String city){
+    public boolean insertHealthUnit(HealthUnit healthUnit){
         SQLiteDatabase database = this.getWritableDatabase();
         ContentValues contentValues = new ContentValues();
-        contentValues.put(HEALTHUNIT_ID, id);
-        contentValues.put(LATITUDE, latitude);
-        contentValues.put(LONGITUDE,longitude);
-        contentValues.put(NAMEHOSPITAL,nameHospital);
-        contentValues.put(UNITTYPE,unitType);
-        contentValues.put(ADDRESSNUMBER,addressNumber);
-        contentValues.put(DISTRICT,district);
-        contentValues.put(STATE,state);
-        contentValues.put(CITY,city);
+        contentValues.put(ZIPCODE, healthUnit.getZipCode());
+        contentValues.put(LATITUDE, healthUnit.getLatitude());
+        contentValues.put(LONGITUDE,healthUnit.getLongitude());
+        contentValues.put(NAMEHOSPITAL,healthUnit.getNameHospital());
+        contentValues.put(UNITTYPE,healthUnit.getUnitType());
+        contentValues.put(DISTRICT,healthUnit.getDistrict());
+        contentValues.put(STATE,healthUnit.getState());
+        contentValues.put(CITY,healthUnit.getCity());
 
         long result = database.insert(HEALTHUNIT_TABLE,null,contentValues);
         database.close();
--- EmerGo/app/src/main/java/unlv/erc/emergo/model/HealthUnit.java
+++ EmerGo/app/src/main/java/unlv/erc/emergo/model/HealthUnit.java
@@ -7,32 +7,31 @@ public class HealthUnit {
     private Double longitude;
     private String nameHospital;
     private String unitType;
-    private String addressNumber;
+    private Integer zipCode;
     private String district;
     private String state;
     private String city;
     private Float distance;
-    private Integer id;
+
 
     public HealthUnit(){
 
     }
 
-    public HealthUnit(Integer id,Double latitude, Double longitude, String nameHospital,
-                      String unitType,String addressNumber, String district, String state,
+    public HealthUnit(Integer zipCode,Double latitude, Double longitude, String nameHospital,
+                      String unitType,String district, String state,
                       String city) {
-        setId(id);
+        setZipCode(zipCode);
         setLatitude(latitude);
         setLongitude(longitude);
         setNameHospital(nameHospital);
         setUnitType(unitType);
-        setAddressNumber(addressNumber);
         setDistrict(district);
         setState(state);
         setCity(city);
     }
 
-    public HealthUnit (Double latitude, Double longitude, String nameHospital, String unitType,
+    /*public HealthUnit (Double latitude, Double longitude, String nameHospital, String unitType,
                        String addressNumber, String district, String state, String city) {
         setLatitude(latitude);
         setLongitude(longitude);
@@ -42,15 +41,7 @@ public class HealthUnit {
         setDistrict(district);
         setState(state);
         setCity(city);
-    }
-
-    public Integer getId() {
-        return id;
-    }
-
-    public void setId(Integer id) {
-        this.id = id;
-    }
+    }*/
 
     public Double getLatitude() {
         return latitude;
@@ -84,12 +75,12 @@ public class HealthUnit {
         this.unitType = unitType;
     }
 
-    public String getAddressNumber() {
-        return addressNumber;
+    public Integer getZipCode() {
+        return zipCode;
     }
 
-    public void setAddressNumber(String addressNumber) {
-        this.addressNumber = addressNumber;
+    public void setZipCode(Integer zipCode) {
+        this.zipCode = zipCode;
     }
 
     public String getDistrict() {
