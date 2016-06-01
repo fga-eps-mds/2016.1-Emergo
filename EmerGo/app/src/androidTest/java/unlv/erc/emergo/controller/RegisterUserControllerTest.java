75cb7ff2e21a5dcfbe60c5927266fc276e874794
MrVictor42 <guin-hu@hotmail.com>
2016-06-01 12:30:52 -0300

    more test

--- EmerGo/app/src/androidTest/java/unlv/erc/emergo/controller/RegisterUserControllerTest.java
+++ EmerGo/app/src/androidTest/java/unlv/erc/emergo/controller/RegisterUserControllerTest.java
@@ -54,15 +54,166 @@ public class RegisterUserControllerTest extends ActivityInstrumentationTestCase2
         onView(withId(R.id.birthdayTextView)).check(matches(withText("Data de Nascimento")));
     }
 
-    public void testDateField(){
+    public void testBirthdayField(){
         openActionBarOverflowOrOptionsMenu(InstrumentationRegistry.getTargetContext());
         onView(withText("Salvar")).perform(click());
-        onView(withId(R.id.birthdayEditText)).check(matches(isDisplayed()));
-        onView(withId(R.id.birthdayEditText)).perform(typeText("09/07/1995"));
+        onView(withId(R.id.birthdayTextView)).check(matches(isDisplayed()));
+        onView(withId(R.id.birthdayEditText)).perform(typeText("14/03/1912"));
         onView(withId(R.id.birthdayEditText)).perform(click());
-        onView(withId(R.id.birthdayEditText)).check(matches(withText("09/07/1995")));
+        onView(withId(R.id.birthdayEditText)).check(matches(withText("14/03/1912")));
     }
 
+    public void testIfTypeBloodLabelIsCorrect() {
+        openActionBarOverflowOrOptionsMenu(InstrumentationRegistry.getTargetContext());
+        onView(withText("Salvar")).perform(click());
+        onView(withId(R.id.typeBloodTextView)).check(matches(isDisplayed()));
+        onView(withId(R.id.typeBloodTextView)).check(matches(withText("Tipo Sanguíneo")));
+    }
+
+    public void testTypeBloodField(){
+        openActionBarOverflowOrOptionsMenu(InstrumentationRegistry.getTargetContext());
+        onView(withText("Salvar")).perform(click());
+        onView(withId(R.id.typeBloodTextView)).check(matches(isDisplayed()));
+        onView(withId(R.id.typeBloodSpinner)).perform(typeText("AB+"));
+        onView(withId(R.id.typeBloodSpinner)).perform(click());
+        onView(withId(R.id.typeBloodSpinner)).check(matches(withText("AB+")));
+    }
+
+    public void testIfCardiacLabelIsCorrect() {
+        openActionBarOverflowOrOptionsMenu(InstrumentationRegistry.getTargetContext());
+        onView(withText("Salvar")).perform(click());
+        onView(withId(R.id.cardiacTextView)).check(matches(isDisplayed()));
+        onView(withId(R.id.cardiacTextView)).check(matches(withText("Cardiaco")));
+    }
+
+    public void testCardiacFieldOptionYes(){
+        openActionBarOverflowOrOptionsMenu(InstrumentationRegistry.getTargetContext());
+        onView(withText("Salvar")).perform(click());
+        onView(withId(R.id.cardiacTextView)).check(matches(isDisplayed()));
+        onView(withId(R.id.cardiacSpinner)).perform(typeText("Sim"));
+        onView(withId(R.id.cardiacSpinner)).perform(click());
+        onView(withId(R.id.cardiacSpinner)).check(matches(withText("Sim")));
+    }
+
+    public void testCardiacFieldOptionNo(){
+        openActionBarOverflowOrOptionsMenu(InstrumentationRegistry.getTargetContext());
+        onView(withText("Salvar")).perform(click());
+        onView(withId(R.id.cardiacTextView)).check(matches(isDisplayed()));
+        onView(withId(R.id.cardiacSpinner)).perform(typeText("Não"));
+        onView(withId(R.id.cardiacSpinner)).perform(click());
+        onView(withId(R.id.cardiacSpinner)).check(matches(withText("Não")));
+    }
+
+    public void testCardiacFieldOptionIDontKnow(){
+        openActionBarOverflowOrOptionsMenu(InstrumentationRegistry.getTargetContext());
+        onView(withText("Salvar")).perform(click());
+        onView(withId(R.id.cardiacTextView)).check(matches(isDisplayed()));
+        onView(withId(R.id.cardiacSpinner)).perform(typeText("Não Sei"));
+        onView(withId(R.id.cardiacSpinner)).perform(click());
+        onView(withId(R.id.cardiacSpinner)).check(matches(withText("Não Sei")));
+    }
+
+    public void testIfDiabeticLabelIsCorrect() {
+        openActionBarOverflowOrOptionsMenu(InstrumentationRegistry.getTargetContext());
+        onView(withText("Salvar")).perform(click());
+        onView(withId(R.id.diabeticTextView)).check(matches(isDisplayed()));
+        onView(withId(R.id.diabeticTextView)).check(matches(withText("Diabetico")));
+    }
+
+    public void testDiabeticFieldOptionYes(){
+        openActionBarOverflowOrOptionsMenu(InstrumentationRegistry.getTargetContext());
+        onView(withText("Salvar")).perform(click());
+        onView(withId(R.id.diabeticTextView)).check(matches(isDisplayed()));
+        onView(withId(R.id.diabeticSpinner)).perform(typeText("Sim"));
+        onView(withId(R.id.diabeticSpinner)).perform(click());
+        onView(withId(R.id.diabeticSpinner)).check(matches(withText("Sim")));
+    }
+
+    public void testDiabeticFieldOptionNo(){
+        openActionBarOverflowOrOptionsMenu(InstrumentationRegistry.getTargetContext());
+        onView(withText("Salvar")).perform(click());
+        onView(withId(R.id.diabeticTextView)).check(matches(isDisplayed()));
+        onView(withId(R.id.diabeticSpinner)).perform(typeText("Não"));
+        onView(withId(R.id.diabeticSpinner)).perform(click());
+        onView(withId(R.id.diabeticSpinner)).check(matches(withText("Não")));
+    }
+
+    public void testDiabeticFieldOptionIDontKnow(){
+        openActionBarOverflowOrOptionsMenu(InstrumentationRegistry.getTargetContext());
+        onView(withText("Salvar")).perform(click());
+        onView(withId(R.id.diabeticTextView)).check(matches(isDisplayed()));
+        onView(withId(R.id.diabeticSpinner)).perform(typeText("Não Sei"));
+        onView(withId(R.id.diabeticSpinner)).perform(click());
+        onView(withId(R.id.diabeticSpinner)).check(matches(withText("Não Sei")));
+    }
+
+    public void testIfHypertensionLabelIsCorrect() {
+        openActionBarOverflowOrOptionsMenu(InstrumentationRegistry.getTargetContext());
+        onView(withText("Salvar")).perform(click());
+        onView(withId(R.id.hipertensionTextView)).check(matches(isDisplayed()));
+        onView(withId(R.id.hipertensionTextView)).check(matches(withText("Hipertenso")));
+    }
+
+    public void testHypertensionFieldOptionYes(){
+        openActionBarOverflowOrOptionsMenu(InstrumentationRegistry.getTargetContext());
+        onView(withText("Salvar")).perform(click());
+        onView(withId(R.id.hipertensionTextView)).check(matches(isDisplayed()));
+        onView(withId(R.id.hipertensionSpinner)).perform(typeText("Sim"));
+        onView(withId(R.id.hipertensionSpinner)).perform(click());
+        onView(withId(R.id.hipertensionSpinner)).check(matches(withText("Sim")));
+    }
+
+    public void testHypertensionFieldOptionNo(){
+        openActionBarOverflowOrOptionsMenu(InstrumentationRegistry.getTargetContext());
+        onView(withText("Salvar")).perform(click());
+        onView(withId(R.id.hipertensionTextView)).check(matches(isDisplayed()));
+        onView(withId(R.id.hipertensionSpinner)).perform(typeText("Não"));
+        onView(withId(R.id.hipertensionSpinner)).perform(click());
+        onView(withId(R.id.hipertensionSpinner)).check(matches(withText("Não")));
+    }
+
+    public void testHypertensionFieldOptionIDontKnow(){
+        openActionBarOverflowOrOptionsMenu(InstrumentationRegistry.getTargetContext());
+        onView(withText("Salvar")).perform(click());
+        onView(withId(R.id.hipertensionTextView)).check(matches(isDisplayed()));
+        onView(withId(R.id.hipertensionSpinner)).perform(typeText("Não Sei"));
+        onView(withId(R.id.hipertensionSpinner)).perform(click());
+        onView(withId(R.id.hipertensionSpinner)).check(matches(withText("Não Sei")));
+    }
+
+    public void testIfSeropositiveLabelIsCorrect() {
+        openActionBarOverflowOrOptionsMenu(InstrumentationRegistry.getTargetContext());
+        onView(withText("Salvar")).perform(click());
+        onView(withId(R.id.seropositiveTextView)).check(matches(isDisplayed()));
+        onView(withId(R.id.seropositiveTextView)).check(matches(withText("Soropositivo")));
+    }
+
+    public void testSeropositiveFieldOptionYes(){
+        openActionBarOverflowOrOptionsMenu(InstrumentationRegistry.getTargetContext());
+        onView(withText("Salvar")).perform(click());
+        onView(withId(R.id.seropositiveTextView)).check(matches(isDisplayed()));
+        onView(withId(R.id.soropositiveSpinner)).perform(typeText("Sim"));
+        onView(withId(R.id.soropositiveSpinner)).perform(click());
+        onView(withId(R.id.soropositiveSpinner)).check(matches(withText("Sim")));
+    }
+
+    public void testSeropositiveFieldOptionNo(){
+        openActionBarOverflowOrOptionsMenu(InstrumentationRegistry.getTargetContext());
+        onView(withText("Salvar")).perform(click());
+        onView(withId(R.id.seropositiveTextView)).check(matches(isDisplayed()));
+        onView(withId(R.id.soropositiveSpinner)).perform(typeText("Não"));
+        onView(withId(R.id.soropositiveSpinner)).perform(click());
+        onView(withId(R.id.soropositiveSpinner)).check(matches(withText("Não")));
+    }
+
+    public void testSeropositiveFieldOptionIDontKnow(){
+        openActionBarOverflowOrOptionsMenu(InstrumentationRegistry.getTargetContext());
+        onView(withText("Salvar")).perform(click());
+        onView(withId(R.id.seropositiveTextView)).check(matches(isDisplayed()));
+        onView(withId(R.id.soropositiveSpinner)).perform(typeText("Não Sei"));
+        onView(withId(R.id.soropositiveSpinner)).perform(click());
+        onView(withId(R.id.soropositiveSpinner)).check(matches(withText("Não Sei")));
+    }
 
     public void testUpdateOption(){
         openActionBarOverflowOrOptionsMenu(InstrumentationRegistry.getTargetContext());
@@ -77,14 +228,4 @@ public class RegisterUserControllerTest extends ActivityInstrumentationTestCase2
         onView(withId(R.id.deleteButton)).check(matches(isDisplayed()));
         onView(withId(R.id.deleteButton)).check(matches(withText("Excluir")));
     }
-
-    public void testBirthday(){
-        openActionBarOverflowOrOptionsMenu(InstrumentationRegistry.getTargetContext());
-        onView(withText("Salvar")).perform(click());
-        onView(withId(R.id.birthdayTextView)).check(matches(isDisplayed()));
-        onView(withId(R.id.birthdayEditText)).perform(typeText("14/03/1912"));
-        onView(withId(R.id.birthdayEditText)).perform(click());
-        onView(withId(R.id.birthdayEditText)).check(matches(withText("14/03/1912")));
-    }
-
 }
