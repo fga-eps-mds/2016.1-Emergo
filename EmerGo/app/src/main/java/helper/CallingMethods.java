1271f31ff36209f8cc8df510536a7c2b3b969d10
gabrielrf97 <gabrielrodriguesfaria97@gmail.com>
2016-06-04 01:39:09 -0300

    method created to listen the calls state

--- EmerGo/app/src/main/java/helper/CallingMethods.java
+++ EmerGo/app/src/main/java/helper/CallingMethods.java
@@ -4,12 +4,20 @@ import android.app.Service;
 import android.content.Context;
 import android.content.Intent;
 
-public class CallMethods{
+public class CallingMethods{
 
     public void call(View view){
         Intent callIntent = new Intent(Intent.ACTION_CALL);
         callIntent.setData(Uri.parse("tel:192"));
         startActivity(callIntent);
+    }
+
+    public TelephonyManager listeningCallState(){
+        CallListener phoneListener = new CallListener();
+        TelephonyManager telephonyManager = (TelephonyManager) this
+                .getSystemService(Context.TELEPHONY_SERVICE);
+        telephonyManager.listen(phoneListener,PhoneStateListener.LISTEN_CALL_STATE);
+        return telephonyManager;
     }
 
 }
