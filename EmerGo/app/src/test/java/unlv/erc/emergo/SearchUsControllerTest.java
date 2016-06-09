dea91125c42ef76be985451dbed7fc4e41b514fa
André Bedran <bedran.fleck@gmail.com>
2016-06-09 17:57:48 -0300

    Adding first unit test for SearchController

---
+++ EmerGo/app/src/test/java/unlv/erc/emergo/SearchUsControllerTest.java
@@ -0,0 +1,26 @@
+package unlv.erc.emergo;
+
+import android.content.Context;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.Mock;
+import org.mockito.Mockito.*;
+import org.mockito.runners.MockitoJUnitRunner;
+
+/**
+ * Created by AndreBedran on 6/9/16.
+ */
+
+@RunWith(MockitoJUnitRunner.class)
+public class SearchUsControllerTest {
+
+    @Mock
+    Context aContext;
+
+    @Test
+    public void testGetSearchUsList() {
+
+    }
+
+}
