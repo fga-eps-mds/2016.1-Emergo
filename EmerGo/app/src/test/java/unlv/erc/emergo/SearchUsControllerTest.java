5037d26c0f16a5a9dc4a508a6f9f47e5da4de712
André Bedran <bedran.fleck@gmail.com>
2016-06-10 00:27:21 -0300

    Saving work in progress. unit tests not working.

--- EmerGo/app/src/test/java/unlv/erc/emergo/SearchUsControllerTest.java
+++ EmerGo/app/src/test/java/unlv/erc/emergo/SearchUsControllerTest.java
@@ -2,24 +2,47 @@ package unlv.erc.emergo;
 
 import android.content.Context;
 
+import junit.framework.TestCase;
+
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.Mock;
 import org.mockito.Mockito.*;
 import org.mockito.runners.MockitoJUnitRunner;
 
+import java.util.ArrayList;
+
+import unlv.erc.emergo.controller.HealthUnitController;
+import unlv.erc.emergo.controller.SearchUsController;
+import unlv.erc.emergo.model.HealthUnit;
+
 /**
  * Created by AndreBedran on 6/9/16.
  */
 
 @RunWith(MockitoJUnitRunner.class)
-public class SearchUsControllerTest {
+public class SearchUsControllerTest extends TestCase{
 
     @Mock
     Context aContext;
 
     @Test
     public void testGetSearchUsList() {
+
+        boolean hasPassed = false;
+        HealthUnitController healthUnitController = new HealthUnitController(aContext);
+        ArrayList<HealthUnit> array = healthUnitController.getSearchsUs();
+        SearchUsController searchUsController = new SearchUsController();
+
+        ArrayList<String> testArray = searchUsController.getSearchsUs(array);
+
+        if (testArray.size() > 0){
+            hasPassed = true;
+        } else {
+            //Nothing to do
+        }
+
+        assertTrue(hasPassed);
 
     }
 
