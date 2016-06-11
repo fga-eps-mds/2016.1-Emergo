0ad5a4256e651e47033216fb8f273b4855c0d784
André Bedran <bedran.fleck@gmail.com>
2016-06-11 15:30:35 -0300

    Adding more UI tests. Correcting old ones.

--- EmerGo/app/src/androidTest/java/unlv/erc/emergo/controller/SearchUsControllerUITest.java
+++ EmerGo/app/src/androidTest/java/unlv/erc/emergo/controller/SearchUsControllerUITest.java
@@ -2,8 +2,10 @@ package unlv.erc.emergo.controller;
 
 import android.support.test.espresso.Espresso.*;
 import android.support.test.runner.AndroidJUnit4;
+import android.test.ActivityInstrumentationTestCase2;
 import android.test.suitebuilder.annotation.LargeTest;
 
+import org.junit.Before;
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -14,6 +16,9 @@ import static android.support.test.espresso.Espresso.onView;
 import static android.support.test.espresso.action.ViewActions.closeSoftKeyboard;
 import static android.support.test.espresso.action.ViewActions.typeText;
 import static android.support.test.espresso.assertion.ViewAssertions.matches;
+import static android.support.test.espresso.matcher.ViewMatchers.isDisplayed;
+import static android.support.test.espresso.matcher.ViewMatchers.isEnabled;
+import static android.support.test.espresso.matcher.ViewMatchers.withHint;
 import static android.support.test.espresso.matcher.ViewMatchers.withId;
 import static android.support.test.espresso.matcher.ViewMatchers.withText;
 
@@ -21,21 +26,42 @@ import static android.support.test.espresso.matcher.ViewMatchers.withText;
 /**
  * Created by AndreBedran on 6/10/16.
  */
-@RunWith(AndroidJUnit4.class)
-public class SearchUsControllerUITest {
 
-    @Rule
-    public ActivityTestRule
+public class SearchUsControllerUITest extends ActivityInstrumentationTestCase2<SearchUsController>{
 
-    @Test
-    public void testSearchTypeField() {
+    public SearchUsControllerUITest() { super(SearchUsController.class); }
 
-        String stringToType = "Espresso";
+    @Before
+    public void setUp() throws Exception {
+        super.setUp();
+        getActivity();
+    }
 
-        onView(withId(R.menu.menu_search))
-                .perform(typeText(stringToType))
-                .perform(closeSoftKeyboard())
-                .check(matches(withText(stringToType)));
+
+
+    public void testSearchMenuAvailability() {
+        onView(withId(R.id.search))
+                .check(matches(isDisplayed()));
+    }
+
+    public void testIfSearchFieldIsEnabled() {
+
+        onView(withId(R.id.search))
+                .check(matches(isEnabled()));
+    }
+
+    public void testSearchFieldReceivesTypeSet() {
+
+        String type = "Espresso";
+
+        onView(withId(R.id.search))
+                .perform(typeText(type))
+                .perform(closeSoftKeyboard());
+
+        //TODO see why doesnt work.
+
+        onView(withId(R.id.search))
+                .check(matches(withText(type)));
 
     }
 }
