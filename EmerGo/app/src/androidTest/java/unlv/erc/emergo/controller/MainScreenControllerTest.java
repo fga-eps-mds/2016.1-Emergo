efb4d98a35bb27b0b8be1b3e28503250806c65af
Matheus F. Pimenta <matheusfp2007@yahoo.com.br>
2016-06-10 23:01:02 -0300

    Create MainScreenControllerTest

--- EmerGo/app/src/androidTest/java/unlv/erc/emergo/controller/MainScreenControllerTest.java
+++ EmerGo/app/src/androidTest/java/unlv/erc/emergo/controller/MainScreenControllerTest.java
@@ -1,66 +1,48 @@
 package unlv.erc.emergo.controller;
 
-import android.support.test.filters.SmallTest;
-import android.support.test.rule.ActivityTestRule;
-import android.support.test.runner.AndroidJUnit4;
+import android.support.test.espresso.matcher.ViewMatchers;
 import android.test.ActivityInstrumentationTestCase2;
-import android.test.InstrumentationTestCase;
-
+import android.test.UiThreadTest;
+import android.widget.Button;
 import static android.support.test.espresso.Espresso.onView;
-import static android.support.test.espresso.Espresso.openActionBarOverflowOrOptionsMenu;
-import static android.support.test.espresso.action.ViewActions.clearText;
+import static android.support.test.espresso.assertion.ViewAssertions.doesNotExist;
+import static android.support.test.espresso.matcher.ViewMatchers.isCompletelyDisplayed;
+import static android.support.test.espresso.matcher.ViewMatchers.isDisplayed;
+import static android.support.test.espresso.matcher.ViewMatchers.isEnabled;
+import static android.support.test.espresso.matcher.ViewMatchers.withId;
 import static android.support.test.espresso.action.ViewActions.click;
 import static android.support.test.espresso.action.ViewActions.typeText;
-import static android.support.test.espresso.assertion.ViewAssertions.doesNotExist;
 import static android.support.test.espresso.assertion.ViewAssertions.matches;
-import static android.support.test.espresso.matcher.ViewMatchers.hasErrorText;
-import static android.support.test.espresso.matcher.ViewMatchers.isDisplayed;
-import static android.support.test.espresso.matcher.ViewMatchers.withId;
 import static android.support.test.espresso.matcher.ViewMatchers.withText;
 
-import org.junit.Assert;
-import org.junit.Rule;
-import org.junit.Test;
-import org.junit.runner.RunWith;
+import org.junit.Before;
 
 import unlv.erc.emergo.R;
 
-
-@RunWith(AndroidJUnit4.class)
-@SmallTest
-public class MainScreenControllerTest extends ActivityInstrumentationTestCase2<MainScreenController>{
-
-    MainScreenController tela = new MainScreenController();
+public class MainScreenControllerTest extends ActivityInstrumentationTestCase2<MainScreenController> {
 
     public MainScreenControllerTest(){
         super(MainScreenController.class);
     }
 
-    public void testGoButtonClickability(){
-        onView(withId(tela.goButton.getId())).perform(click());
+    @Before
+    public void setUp() throws Exception{
+        super.setUp();
+        getActivity().getCallingActivity();
+    }
+
+    public void testButtonGoClick(){
+
+        onView(ViewMatchers.withId(R.id.buttonGo)).check(matches(isDisplayed()));
+        onView(withText("GO!")).perform(click());
+        onView(withId(R.id.buttonGo)).check(matches(withText("GO!")));
     }
 
 
-    public void testOkButtonClickability(){
-        onView(withId(tela.fineButton.getId())).perform(click());
+    public void testButtonOkayClick(){
+
+        onView(withId(R.id.buttonOkay)).check(matches(isDisplayed()));
+        onView(withText("Estou Bem")).perform(click());
+        onView(withId(R.id.buttonOkay)).check(matches(withText("Estou Bem")));
     }
-
-    public void testGoButtonVisibility(){
-
-        MainScreenController tela = new MainScreenController();
-
-        assertTrue(tela.goButton.isEnabled());
-
-    }
-
-
-    public void testOkButtonVisibility(){
-
-        MainScreenController tela = new MainScreenController();
-
-        assertTrue(tela.fineButton.isEnabled());
-
-    }
-
-
 }
