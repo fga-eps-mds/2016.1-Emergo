be09cc6158b748f329a5fc7568dba0d63d81ae6d
André Bedran <bedran.fleck@gmail.com>
2016-06-11 17:02:37 -0300

    Finishing UI tests for SearchController.

--- EmerGo/app/src/androidTest/java/unlv/erc/emergo/controller/SearchUsControllerUITest.java
+++ EmerGo/app/src/androidTest/java/unlv/erc/emergo/controller/SearchUsControllerUITest.java
@@ -4,6 +4,8 @@ import android.support.test.espresso.Espresso.*;
 import android.support.test.runner.AndroidJUnit4;
 import android.test.ActivityInstrumentationTestCase2;
 import android.test.suitebuilder.annotation.LargeTest;
+import android.view.View;
+import android.widget.ArrayAdapter;
 
 import org.junit.Before;
 import org.junit.Rule;
@@ -12,15 +14,23 @@ import org.junit.runner.RunWith;
 
 import unlv.erc.emergo.R;
 
+import static android.support.test.espresso.Espresso.onData;
 import static android.support.test.espresso.Espresso.onView;
+import static android.support.test.espresso.action.ViewActions.click;
 import static android.support.test.espresso.action.ViewActions.closeSoftKeyboard;
+import static android.support.test.espresso.action.ViewActions.scrollTo;
 import static android.support.test.espresso.action.ViewActions.typeText;
 import static android.support.test.espresso.assertion.ViewAssertions.matches;
+import static android.support.test.espresso.matcher.ViewMatchers.hasFocus;
+import static android.support.test.espresso.matcher.ViewMatchers.isClickable;
 import static android.support.test.espresso.matcher.ViewMatchers.isDisplayed;
 import static android.support.test.espresso.matcher.ViewMatchers.isEnabled;
 import static android.support.test.espresso.matcher.ViewMatchers.withHint;
 import static android.support.test.espresso.matcher.ViewMatchers.withId;
 import static android.support.test.espresso.matcher.ViewMatchers.withText;
+import static org.hamcrest.CoreMatchers.allOf;
+import static org.hamcrest.CoreMatchers.instanceOf;
+import static org.hamcrest.core.Is.is;
 
 
 /**
@@ -50,18 +60,37 @@ public class SearchUsControllerUITest extends ActivityInstrumentationTestCase2<S
                 .check(matches(isEnabled()));
     }
 
-    public void testSearchFieldReceivesTypeSet() {
+    public void testIfSearchMenuIsClickable() {
+        onView(withId(R.id.search))
+                .perform(click());
+    }
 
-        String type = "Espresso";
+    public void testIfFieldReceivesText() {
 
         onView(withId(R.id.search))
-                .perform(typeText(type))
-                .perform(closeSoftKeyboard());
+                .perform(click());
 
-        //TODO see why doesnt work.
+        View aView = getActivity().getCurrentFocus();
+
+        onView(withId(aView.getId()))
+                .perform(typeText("Hos"));
+
+
+    }
+
+    public void testIfResultsAreShown() {
 
         onView(withId(R.id.search))
-                .check(matches(withText(type)));
+                .perform(click());
+
+        View aView = getActivity().getCurrentFocus();
+
+        onView(withId(aView.getId()))
+                .perform(typeText("Hos"));
+
+        onView(withId(R.id.list_of_search_us))
+                .check(matches(isDisplayed()));
+
 
     }
 }
