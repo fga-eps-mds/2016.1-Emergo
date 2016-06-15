33286002cf0bbdfb3b93e3df9aa7f22892082519
André Bedran <bedran.fleck@gmail.com>
2016-06-14 22:40:12 -0300

    Moving old unit test classes to the right package.

---
+++ EmerGo/app/src/test/java/unlv/erc/emergo/EmergencyContactTest.java
@@ -0,0 +1,30 @@
+package unlv.erc.emergo;
+
+import android.test.InstrumentationTestCase;
+
+import unlv.erc.emergo.model.EmergencyContact;
+
+/**
+ * Created by Matheus on 27/05/2016.
+ */
+public class EmergencyContactTest extends InstrumentationTestCase {
+
+    private EmergencyContact contact;
+
+    public void testGetNameContact(){
+        contact = new EmergencyContact();
+        String name = "Astolfo";
+        contact.setName("Astolfo");
+
+        assertEquals(name,contact.getName());
+
+    }
+
+    public void testSetNameContact(){
+        contact = new EmergencyContact();
+        contact.setName("Jovaildo");
+
+        assertEquals("Jovaildo",contact.getName());
+
+    }
+}
