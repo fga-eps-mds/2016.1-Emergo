a58c85348d8a5b0068b645140958f7132f253845
MrVictor42 <guin-hu@hotmail.com>
2016-06-15 17:02:19 -0300

    notification

--- EmerGo/app/src/main/java/unlv/erc/emergo/controller/MainScreenController.java
+++ EmerGo/app/src/main/java/unlv/erc/emergo/controller/MainScreenController.java
@@ -1,12 +1,13 @@
 package unlv.erc.emergo.controller;
 
 import android.app.Activity;
-import android.app.Notification;
 import android.app.NotificationManager;
 import android.app.PendingIntent;
+import android.app.TaskStackBuilder;
 import android.content.Intent;
 import android.database.Cursor;
 import android.os.Bundle;
+import android.support.v7.app.NotificationCompat;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.Button;
@@ -27,6 +28,8 @@ public class MainScreenController extends Activity {
     private DataAccessObject dataAccessObject = new DataAccessObject(this);
     private Cursor result;
     UserDao myDatabase;
+    private final int MAXIMUMARRAY = 7;
+    private int number = 0;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -69,19 +72,45 @@ public class MainScreenController extends Activity {
 
     private void medicalRecordsNotification(){
         result.moveToFirst();
-        Intent intent = new Intent(this,MainScreenController.class);
-        PendingIntent pendingIntent = PendingIntent.getActivity(this,0,intent,0);
 
-        Notification notification = new Notification.Builder(this)
-                .setTicker("Ficha Médica")
-                .setContentTitle("Ficha Médica")
-                .setContentText("Nome: "+result.getString(1))
-                .setSmallIcon(R.drawable.button_go)
-                .setContentIntent(pendingIntent).getNotification();
-        notification.flags = Notification.FLAG_AUTO_CANCEL;
+        NotificationCompat.Builder notification = new NotificationCompat.Builder(this);
+
+        notification.setContentTitle("Ficha Médica");
+        notification.setContentText("Você tem uma ficha médica!");
+        notification.setTicker("Alerta de Mensagem");
+        notification.setSmallIcon(R.drawable.icon_emergo);
+
+        notification.setNumber(++number);
+
+        NotificationCompat.InboxStyle inboxStyle = new NotificationCompat.InboxStyle();
+
+        String events[] = new String[7];
+
+        events[0] = new String("Nome: "+result.getString(1));
+        events[1] = new String("Data de Nascimento: "+result.getString(2));
+        events[2] = new String("Tipo Sanguineo: "+result.getString(3));
+        events[3] = new String("Cardiaco: "+result.getString(4));
+        events[4] = new String("Diabetico: "+result.getString(5));
+        events[5] = new String("Hipertenso: "+result.getString(6));
+        events[6] = new String("Soropositivo: "+result.getString(7));
+        events[6] = new String("Observações Especiais: "+result.getString(8));
+
+        inboxStyle.setBigContentTitle("Ficha Médica");
+
+        for(int aux=0;aux<MAXIMUMARRAY;aux++){
+            inboxStyle.addLine(events[aux]);
+        }
+        notification.setStyle(inboxStyle);
+
+        Intent resultIntent = new Intent(this,MainScreenController.class);
+        TaskStackBuilder stackBuilder = TaskStackBuilder.create(this);
+        stackBuilder.addParentStack(MainScreenController.class);
+        stackBuilder.addNextIntent(resultIntent);
+        PendingIntent resultPedindIntent = stackBuilder.getPendingIntent(0,PendingIntent.FLAG_UPDATE_CURRENT);
+        notification.setContentIntent(resultPedindIntent);
 
         NotificationManager notificationManager = (NotificationManager) getSystemService(NOTIFICATION_SERVICE);
 
-        notificationManager.notify(0,notification);
+        notificationManager.notify(0,notification.build());
     }
 }
