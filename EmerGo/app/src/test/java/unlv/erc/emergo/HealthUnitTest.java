149d2eaaafa03764d04d0c704ad437a2e4cb1fa8
André Bedran <bedran.fleck@gmail.com>
2016-06-18 01:29:32 -0300

    Testing HealthUnit getters

--- EmerGo/app/src/test/java/unlv/erc/emergo/HealthUnitTest.java
+++ EmerGo/app/src/test/java/unlv/erc/emergo/HealthUnitTest.java
@@ -6,6 +6,7 @@ import org.junit.Test;
 import unlv.erc.emergo.model.HealthUnit;
 
 import static junit.framework.Assert.assertNotNull;
+import static org.junit.Assert.assertEquals;
 
 
 public class HealthUnitTest {
@@ -32,6 +33,24 @@ public class HealthUnitTest {
         assertNotNull(healthUnit);
 
     }
+
+    @Test
+    public void testGetters() {
+
+        String stringTest = "asd";
+        healthUnit = new HealthUnit(0.0, 0.0, stringTest, stringTest, stringTest, stringTest, stringTest, stringTest);
+
+        assertEquals(healthUnit.getLatitude(), (Double) 0.0);
+        assertEquals(healthUnit.getLongitude(), (Double) 0.0);
+        assertEquals(healthUnit.getAddressNumber(), stringTest);
+        assertEquals(healthUnit.getCity(), stringTest);
+        assertEquals(healthUnit.getDistrict(), stringTest);
+        assertEquals(healthUnit.getNameHospital(), stringTest);
+        assertEquals(healthUnit.getState(), stringTest);
+        assertEquals(healthUnit.getUnitType(), stringTest);
+
+    }
+
 
 
 
