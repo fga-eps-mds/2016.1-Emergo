496ffe83d61c954d3d0ed1bc3bf471872a1b2537
naiieandrade <andradenaiara@hotmail.com>
2016-06-18 01:07:06 -0300

    "main screen controller test"
    Signed-off-by: naiieandrade <andradenaiara@hotmail.com>

---
+++ EmerGo/app/src/androidTest/java/unlv/erc/emergo/controller/MainScreenControllerTest.java
@@ -0,0 +1,43 @@
+package unlv.erc.emergo.controller;
+
+import android.support.test.uiautomator.UiDevice;
+import android.test.ActivityInstrumentationTestCase2;
+import org.junit.Before;
+
+import dao.EmergencyContactDao;
+import unlv.erc.emergo.R;
+
+import static android.support.test.espresso.Espresso.onView;
+import static android.support.test.espresso.assertion.ViewAssertions.matches;
+import static android.support.test.espresso.matcher.ViewMatchers.isClickable;
+import static android.support.test.espresso.matcher.ViewMatchers.isDisplayed;
+import static android.support.test.espresso.matcher.ViewMatchers.withId;
+import static android.support.test.espresso.matcher.ViewMatchers.withText;
+import static android.support.test.espresso.action.ViewActions.click;
+
+
+public class MainScreenControllerTest extends ActivityInstrumentationTestCase2<MainScreenController> {
+    private UiDevice device;
+    private EmergencyContactDao emergencyContactDao;
+
+    public MainScreenControllerTest() {
+        super(MainScreenController.class);
+    }
+
+    @Before
+    public void setUp() throws Exception{
+        super.setUp();
+        getActivity();
+        device = UiDevice.getInstance(getInstrumentation());
+    }
+
+    public void testButtonGo(){
+        onView(withId(R.id.buttonGo)).check(matches(isDisplayed()));
+        onView(withText("GO!")).perform(click());
+        onView(withId(R.id.buttonGo)).check(matches(withText("GO!")));
+
+    }
+
+
+
+}
