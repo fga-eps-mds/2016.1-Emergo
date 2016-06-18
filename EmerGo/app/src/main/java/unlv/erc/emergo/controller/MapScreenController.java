cc136e6832180b27a25dd6aa0a8bb4ca3cfac108
gabrielrf97 <gabrielrodriguesfaria97@gmail.com>
2016-06-17 21:45:38 -0300

    Sms method on Map Screen

--- EmerGo/app/src/main/java/unlv/erc/emergo/controller/MapScreenController.java
+++ EmerGo/app/src/main/java/unlv/erc/emergo/controller/MapScreenController.java
@@ -1,8 +1,11 @@
 package unlv.erc.emergo.controller;
 
 import android.content.Intent;
+import android.database.Cursor;
 import android.os.Bundle;
 import android.support.v4.app.FragmentActivity;
+import android.telephony.SmsManager;
+import android.util.Log;
 import android.view.View;
 import android.widget.Toast;
 
@@ -14,6 +17,8 @@ import com.google.android.gms.maps.model.BitmapDescriptorFactory;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
 
+import dao.DataAccessObject;
+import dao.EmergencyContactDao;
 import helper.GPSTracker;
 import helper.Services;
 import unlv.erc.emergo.R;
@@ -22,6 +27,8 @@ public class MapScreenController extends FragmentActivity implements OnMapReadyC
 
     private GoogleMap mMap;
     private Services services = new Services();
+    EmergencyContactDao  emergencyContactDao = new EmergencyContactDao(this);
+
 
 
     @Override
@@ -37,10 +44,25 @@ public class MapScreenController extends FragmentActivity implements OnMapReadyC
     }
 
     public void goClicked(View map_screen){
-        Toast.makeText(this , "Função não habilitada!" , Toast.LENGTH_SHORT).show();
+        Cursor result = emergencyContactDao.getEmergencyContact();
+        Log.i("NUM CONTATOS: ",result.getCount()+" ");
+
+        if(result.getCount() != 0) {
+            try {
+                while(result.moveToNext()){
+                    SmsManager.getDefault().sendTextMessage(result.getString(2), null, result.getString(1) + ", Estou precisando de ajuda urgente! teste EmerGO", null, null);
+                }
+                Toast.makeText(getApplicationContext(),"Ajuda a caminho", Toast.LENGTH_LONG).show();
+            }catch(Exception exception){
+                Toast.makeText(getApplicationContext(),"Impossível encaminhar o SMS", Toast.LENGTH_LONG).show();
+            }
+        }else{
+            Toast.makeText(getApplicationContext(),"Nenhum contato adicionado", Toast.LENGTH_LONG).show();
+        }
+/*
         Intent routeActivity = new Intent();
         routeActivity.setClass(this, RouteActivity.class);
-        startActivity(routeActivity);
+        startActivity(routeActivity);*/
     }
 
     public void listMapsImageClicked(View map_screen){
