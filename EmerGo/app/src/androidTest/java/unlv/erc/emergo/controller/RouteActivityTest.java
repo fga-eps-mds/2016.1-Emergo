d60375a927059104b799a36e225da39e61e7562b
MrVictor42 <guin-hu@hotmail.com>
2016-06-23 00:04:40 -0300

    tests routeactivity

--- EmerGo/app/src/androidTest/java/unlv/erc/emergo/controller/RouteActivityTest.java
+++ EmerGo/app/src/androidTest/java/unlv/erc/emergo/controller/RouteActivityTest.java
@@ -5,12 +5,7 @@ import android.test.ActivityInstrumentationTestCase2;
 
 import org.junit.Before;
 
-import java.util.ArrayList;
-
-import dao.DataAccessObject;
-import helper.Services;
 import unlv.erc.emergo.R;
-import unlv.erc.emergo.model.HealthUnit;
 
 import static android.support.test.espresso.Espresso.onView;
 import static android.support.test.espresso.action.ViewActions.click;
@@ -20,107 +15,53 @@ import static android.support.test.espresso.matcher.ViewMatchers.withId;
 public class RouteActivityTest extends ActivityInstrumentationTestCase2<RouteActivity> {
 
     private UiDevice device;
-    public RouteActivityTest(){
+
+    public RouteActivityTest() {
         super(RouteActivity.class);
     }
 
     @Before
-    public void setUp() throws  Exception{
+    public void setUp() throws Exception {
         super.setUp();
         getActivity();
         device = UiDevice.getInstance(getInstrumentation());
     }
 
-    public void testIfButtonCancelCallIsWorking(){
+    public void testIfButtonCancelCallIsWorking() {
         onView(withId(R.id.buttonGo)).perform(click());
-
     }
-/////////////////////////////////////////////////////// até aqui funciona ///////////////////////////
-    public void testIfButtonSelfLocationIsWorking(){
 
+    public void testIfButtonSelfLocationIsWorking() {
         onView(withId(R.id.buttonGo)).perform(click());
+        onView(withId(R.id.cancelarLigacao)).perform(click());
         onView(withId(R.id.selfLocation)).perform(click());
+        onView(withId(R.id.buttonGo)).perform(click());
     }
 
-    public void testIfUserInformationIsWorking(){
+    public void testIfUserInformationIsWorking() {
         onView(withId(R.id.buttonGo)).perform(click());
         onView(withId(R.id.userInformation)).perform(click());
     }
 
-    public void testIfGoIsWorking(){
+    public void testIfGoIsWorking() {
+        onView(withId(R.id.buttonGo)).perform(click());
         onView(withId(R.id.cancelarLigacao)).perform(click());
         onView(withId(R.id.buttonGo)).perform(click());
     }
 
-
-
-    public void testIfPhoneIsClickable(){
-        onView(withId(R.id.cancelarLigacao)).perform(click());
-        onView(withId(R.id.phone)).perform(click());
-    }
-
-    public void testIfMapScreenOpens(){
+    public void testConfigOption() {
+        onView(withId(R.id.buttonGo)).perform(click());
         onView(withId(R.id.cancelarLigacao)).perform(click());
         onView(withId(R.id.buttonGo)).perform(click());
+        onView(withId(R.id.iconMenu)).perform(click());
     }
 
-
-
-
-    /*public void testSetUserLocationButton(){
-        onView(withId(R.id.button)).check(matches(isDisplayed()));
-        onView(withText("YOU")).perform(click());
-        onView(withId(R.id.button)).check(matches(withText("YOU")));
+    public void testListsUs(){
+        onView(withId(R.id.buttonGo)).perform(click());
+        onView(withId(R.id.cancelarLigacao)).perform(click());
+        onView(withId(R.id.buttonGo)).perform(click());
+        onView(withId(R.id.iconList)).perform(click());
+        onView(withId(R.id.buttonGo)).perform(click());
+        onView(withId(R.id.cancelarLigacao)).perform(click());
     }
-
-    public void testIdUserLocationButtonIsClickable(){
-        Button buttonUserLocation = (Button) routeActivity.findViewById(R.id.button);
-        boolean valido = true;
-        if(buttonUserLocation.isClickable()){
-            assertTrue(valido);
-        }else{
-            assertFalse(valido);
-        }
-    }
-
-    public void testIfLabelUserLocationIsCorret(){
-        onView(withId(R.id.button)).check(matches(isDisplayed()));
-        onView(withText("YOU")).perform(click());
-    }
-
-    public void testButtonCancel(){
-        onView(withId(R.id.buttonGo)).check(matches(isDisplayed()));
-        onView(withText("CANCELAR")).perform(click());
-        onView(withId(R.id.buttonGo)).check(matches(withText("CANCELAR")));
-    }
-
-    public void testIfButtonCancelIsClickable(){
-        Button buttonUserLocation = (Button) routeActivity.findViewById(R.id.buttonGo);
-        boolean valido = true;
-        if(buttonUserLocation.isClickable()){
-            assertTrue(valido);
-        }else{
-            assertFalse(valido);
-        }
-    }
-
-    public void testIfLabelCancelIsCorret(){
-        onView(withId(R.id.buttonGo)).check(matches(isDisplayed()));
-        onView(withText("CANCELAR")).perform(click());
-    }*/
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 }
