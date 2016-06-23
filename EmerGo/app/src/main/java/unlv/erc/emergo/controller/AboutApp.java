a471ba1d4be4357421c6c573798393ac96a75ceb
naiieandrade <andradenaiara@hotmail.com>
2016-06-22 22:44:02 -0300

    "editing xml about app"
    Signed-off-by: naiieandrade <andradenaiara@hotmail.com>

--- EmerGo/app/src/main/java/unlv/erc/emergo/controller/AboutApp.java
+++ EmerGo/app/src/main/java/unlv/erc/emergo/controller/AboutApp.java
@@ -16,7 +16,12 @@ public class AboutApp extends Activity {
     protected void onCreate(final Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.about_app);
-        String message= "O aplicativo EmerGo";
+        String message= "\tEmerGo é um aplicativo que oferece a facilidade de " +
+                "encontrar Unidades de Saúde mais próximas. Possui MODO EMERGÊNCIA, " +
+                "que traça a rota para uma Unidade de Saúde mais próxima, " +
+                "ligar para o SAMU, e envia uma mensagem pré definida com pedido de ajuda para" +
+                "os contatos de emergência salvos!" +
+                "\n\n\tTodas as funcionalidades em suas mãos, em apenas um aplicativo.";
 
         informationAboutApp = (TextView) findViewById(R.id.aboutApp);
         informationAboutApp.setText(message);
